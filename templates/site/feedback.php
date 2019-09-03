e72901636186dae4444d0b68ef1a62b587622009
maratgefter <47002971+maratgefter@users.noreply.github.com>
2019-09-03 20:03:04 +0300

    mistake

--- templates/site/feedback.php
+++ templates/site/feedback.php
@@ -1,5 +1,5 @@
 <h1><?=$title?></h1>
-<form action="" method="get">
+<form action="" method="post">
     <input type="text" name="" placeholder="Input your name">
     <textarea name="" cols="30" rows="10" placeholder="Input your feedback"></textarea>
     <input type="button" class="button" value="Send a feedback">
