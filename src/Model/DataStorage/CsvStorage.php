635a3b94fd51bf92bf0645380fceb787c0211cef
maratgefter <47002971+maratgefter@users.noreply.github.com>
2019-09-04 23:32:45 +0300

    add csv

--- src/Model/DataStorage/CsvStorage.php
+++ src/Model/DataStorage/CsvStorage.php
@@ -9,18 +9,23 @@ class CsvStorage extends CrudEntity
     {
 
         $this->checkFileExists();
-        return include($this->file_name);
+        $array = file($this->file_name);
+        print_r($array);
+        $res = [];
 
+        foreach ($data_array as $row) {
+            
+            $csv .= explode(';', $row);
+        }
+        return $res;
     }
 
     function write_file(array $data_array)
     {
         $csv = '';
         foreach ($data_array as $row) {
-            foreach ($row as $value) {
-                $csv .="$value;";
-            }
-            $csv .= '\n';
+            
+            $csv .= implode(';', $row)."\n";
         }
         print_r($csv);
         file_put_contents($this->file_name, $csv);
